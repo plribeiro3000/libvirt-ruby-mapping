6f0c571933ffdd3a8dab52035a5f0ef5b95b56c8
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2012-02-28 21:43:41 -0300

    Added initialize and version methods

--- lib/libvirt-ruby-mapping.rb
+++ lib/libvirt-ruby-mapping.rb
@@ -2,8 +2,19 @@ require "libvirt-ruby-mapping/version"
 
 module Libvirt
   module Ruby
-    module Mapping
-      # Your code goes here...
+    def initialize
+      virInitialize(:int) unless respond_to?(:virInitialize)
+      virInitialize
+    end
+
+    def self.version
+      virGetVersion(:pointer, :string, :pointer, :int) unless respond_to?(:virGetVersion)
+      p = FFI::MemoryPointer.new(:ulong)
+      virGetVersion(p, nil, nil)
+      version = p.get_ulong(0)
+      "#{version / 1_000_000}.#{(version % 1_000_000) / 1_000}.#{(version % 1_000_000) %1000}"
     end
   end
 end
+
+Libvirt::ruby.initialize
